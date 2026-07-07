1a43a788e65d88751340dae4171391617a411ece
MasterALiReza <AliRezanazari998@gmail.com>
2026-07-08 01:42:16 +0330

    Delete opcache_clear.php security utility

--- panel/opcache_clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Force webhook trigger comment
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPCache Cleared Successfully!";
-} else {
-    echo "OPCache extension not available.";
-}
