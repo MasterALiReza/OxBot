0ca24c1db29114f5f4cdb0e360fdf0199f9480c5
MasterALiReza <AliRezanazari998@gmail.com>
2026-07-07 20:44:50 +0330

    Clean up temporary files and OPCache reset lines

--- panel/clear_cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPCache flushed successfully.";
-} else {
-    echo "OPCache is not enabled or function not exists.";
-}
-?>
