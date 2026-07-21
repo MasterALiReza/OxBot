8e11955142ed3ae0a0071f6a1b30451acff556be
MasterALiReza <AliRezanazari998@gmail.com>
2026-07-21 18:57:03 +0330

    chore: enable display errors temporarily for debugging

--- panel/settings_channels.php
+++ panel/settings_channels.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1); error_reporting(E_ALL);
 require_once __DIR__ . '/inc/config.php';
 require_once __DIR__ . '/inc/icons.php';
 require_once __DIR__ . '/../botapi.php';
